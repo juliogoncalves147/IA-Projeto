708666834a8345f5d9d348049d0cfed48655561a
Thatch186 <a93275@alunos.uminho.pt>
2022-01-04 14:25:53 +0000

    Parte de split por distancia com BFs Feita

--- Projeto.pl
+++ Projeto.pl
@@ -340,9 +340,9 @@ buscarPrimeirosXelementos(0,_,[]).
 buscarPrimeirosXelementos(_,[],[]).
 buscarPrimeirosXelementos(N,[H|T],[H|T1]) :- N>=0, N1 is N-1, buscarPrimeirosXelementos(N1,T,T1).
 
-buscarUltimosXelementos(N,L,R):- reverse(L,L1), buscarPrimeirosXelementos(N, L1 ,LR), reverse(LR,R).
+buscarUltimosXelementos(N,L,R):- reverse(L,L1), buscarPrimeirosXelementos(N, L1 ,LR), reverse(LR,R). 
 
-calcDistIdBF(IdA,IdB,Distancia):- getFreguesia(IdA,FA), getFreguesia(IdB,FB), larguraprimeiroBF( FA,FB,_,Distancia).
+calcDistIdBF(IdA,IdB,Distancia):- getFreguesia(IdA,FA), getFreguesia(IdB,FB), larguraprimeiroBF( FA,FB,_,Distancia). %distancia entre duas encomendas
 
 seconds([],[]). %lista de pares -> lista com os segundos elementos de cada par
 seconds([(_,B)|T], [B|Tail]):-seconds(T,Tail).
@@ -352,13 +352,14 @@ ordenaPorDistanciaBF(IdHead, Tail , Ans) :- findall((Distancia,Id),(member(Id,Ta
                                                 seconds(Sorted,Ans). %so testei com o 1,3,10,11
 
 
-splitPorDistancia([H|T],[H|T1],[T2]) :- ordenaPorDistanciaBF(H,T,Lista), %Isto ainda nao funciona
+splitPorDistancia([H|T],[H|T1],T2) :- 
+                        ordenaPorDistanciaBF(H,T,Lista),
                         length(T,Comp),
                         CompTotal is Comp + 1,
-                        mod(CompTotal,2) =:= 0,
-                        PrimeirosElementos is CompTotal / 2 - 1,
-                        buscarPrimeirosXelementos(PrimeirosElementos,Lista,T1),
-                        buscarUltimosXelementos(PrimeirosElementos,Lista,T2).
+                        LenL is div(CompTotal,2) + mod(CompTotal,2) -1 , %-1 porque a head nao conta para ir buscar a lista
+                        LenR is div(CompTotal,2) ,
+                        buscarPrimeirosXelementos(LenL,Lista,T1),
+                        buscarUltimosXelementos(LenR,Lista,T2).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- QUERY 1 - - - - - -  -  -  -  -   -
