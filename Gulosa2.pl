0d767777b38891f0771316a1fe10f40d13adc5ad
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-01-04 21:36:21 +0000

    Gulosa para dois hue

--- Gulosa2.pl
+++ Gulosa2.pl
@@ -1,51 +1,36 @@
-/*
-circuitoGulosa(Nodo,Prazo,Peso,Caminho,bicicleta) :-
-    resolve_gulosa(Nodo,bicicleta,Peso,Caminho),
-    custo(Caminho,Distancia),
-    calculaTempo(bicicleta,Peso,Distancia,Tempo),
-    Tempo =< Prazo.
-circuitoGulosa(Nodo,Prazo,Peso,Caminho,moto) :-
-    resolve_gulosa(Nodo,moto,Peso,Caminho),
-    custo(Caminho,Distancia),
-    calculaTempo(moto,Peso,Distancia,Tempo),
-    Tempo =< Prazo.
-circuitoGulosa(Nodo,Prazo,Peso,Caminho,carro) :-
-    resolve_gulosa(Nodo,carro,Peso,Caminho),
-    custo(Caminho,Distancia),
-    calculaTempo(carro,Peso,Distancia,Tempo),
-    Tempo =< Prazo.
-
-
-
-printCircuito(Caminho,Distancia,TempoEntrega,Transporte) :-
-	write('Caminho = '),writeln(Caminho),
-	write('Distancia = '),write(Distancia),writeln(' km'),
-	write('Tempo de entrega = '),write(TempoEntrega),writeln(' horas'),
-	write('Tipo de transporte = '),writeln(Transporte),writeln('').*/
-%------------------------------------------------------------------------------------------------
-%------------------------------------------------------------------------------------------------
 exactHeuristicCarro(Nodo,Nodo,_,0,0) :- Nodo == Nodo,!.
 exactHeuristicCarro(NodoI,NodoF,Peso,EstimaKm,EstimaTmp) :-
     aprofundamentoProgress(NodoI,NodoF,_,EstimaKm),
     calculaTempo(carro,Peso,EstimaKm,EstimaTmp).
 
+exactHeuristicMoto(Nodo,Nodo,_,0,0) :- Nodo == Nodo,!.
+exactHeuristicMoto(NodoI,NodoF,Peso,EstimaKm,EstimaTmp) :-
+    aprofundamentoProgress(NodoI,NodoF,_,EstimaKm),
+    calculaTempo(moto,Peso,EstimaKm,EstimaTmp).
+
+exactHeuristicBic(Nodo,Nodo,_,0,0) :- Nodo == Nodo,!.
+exactHeuristicBic(NodoI,NodoF,Peso,EstimaKm,EstimaTmp) :-
+    aprofundamentoProgress(NodoI,NodoF,_,EstimaKm),
+    calculaTempo(bicicleta,Peso,EstimaKm,EstimaTmp).
+
+%------------------------------------------------------------------------------------------------
+%------------------------------------------------------------------------------------------------
+
 resolve_gulosa2(NodoI, NodoF, carro, Peso, Caminho) :-
     Peso =< 100,
     exactHeuristicCarro(NodoI,NodoF,Peso,EstimaKm,EstimaTmp),
     agulosaCarro2([[NodoF]/EstimaKm/EstimaTmp], Caminho/_/_, NodoI, Peso).
-/*
+
 resolve_gulosa2(NodoI, NodoF, moto, Peso, Caminho) :-
     Peso =< 20,
-    estimaM(Nodo,EstimaKm,EstimaTmp),
-    agulosaMoto([[Nodo]/EstimaKm/EstimaTmp],InvCaminhoD/_/_),
-    reverse(InvCaminhoD,Caminho).
+    exactHeuristicMoto(NodoI,NodoF,Peso,EstimaKm,EstimaTmp),
+    agulosaMoto2([[NodoF]/EstimaKm/EstimaTmp], Caminho/_/_, NodoI, Peso).
 
 resolve_gulosa2(NodoI, NodoF, bicicleta, Peso, Caminho) :-
     Peso =< 5,
-    estimaB(Nodo,EstimaKm,EstimaTmp),
-    agulosaBic([[Nodo]/EstimaKm/EstimaTmp],InvCaminhoD/_/_),
-    reverse(InvCaminhoD,Caminho).
-*/
+    exactHeuristicBic(NodoI,NodoF,Peso,EstimaKm,EstimaTmp),
+    agulosaBic2([[NodoF]/EstimaKm/EstimaTmp], Caminho/_/_, NodoI, Peso).
+
 
 %------------------------------------------------------------------------------------------------
 %---------------------------------------------  CARRO  ------------------------------------------
@@ -70,62 +55,42 @@ adjacenteCarro2([Nodo|Caminho]/_/_, [ProxNodo,Nodo|Caminho]/EstKm/EstTmp, NodoI,
 
 %------------------------------------------------------------------------------------------------
 %---------------------------------------------  MOTO  ------------------------------------------
-/*
-agulosaMoto(Caminhos,Caminho) :-
+
+agulosaMoto2(Caminhos,Caminho,NodoI,_) :-
     obtem_melhorMoto(Caminhos,Caminho),
-    Caminho = [Nodo|_]/_/_,
-    final(Nodo).
-agulosaMoto(Caminhos,SolucaoCaminho) :-
+    Caminho = [NodoI|_]/_/_.
+agulosaMoto2(Caminhos,SolucaoCaminho,NodoI,Peso) :-
     obtem_melhorMoto(Caminhos,MelhorCaminho),
     seleciona(MelhorCaminho,Caminhos,OutrosCaminhos),
-    expande_gulosaMoto(MelhorCaminho,ExpCaminhos),
+    expande_gulosaMoto2(MelhorCaminho,ExpCaminhos,NodoI,Peso),
     append(OutrosCaminhos,ExpCaminhos,NovoCaminhos),
-    agulosaMoto(NovoCaminhos,SolucaoCaminho).
+    agulosaMoto2(NovoCaminhos,SolucaoCaminho,NodoI,Peso).
 
-expande_gulosaMoto(Caminho,ExpCaminhos) :-
-    findall(NovoCaminho, (adjacenteMoto(Caminho,NovoCaminho)), ExpCaminhos).
+expande_gulosaMoto2(Caminho,ExpCaminhos,NodoI,Peso) :-
+    findall(NovoCaminho, (adjacenteMoto2(Caminho,NovoCaminho,NodoI,Peso)), ExpCaminhos).
 
-adjacenteMoto([Nodo|Caminho]/_/_,[ProxNodo,Nodo|Caminho]/EstKm/EstTmp) :-
+adjacenteMoto2([Nodo|Caminho]/_/_, [ProxNodo,Nodo|Caminho]/EstKm/EstTmp, NodoI, Peso) :-
     move(Nodo,ProxNodo,_),
     \+member(ProxNodo,Caminho),
-    estimaM(ProxNodo,EstKm,EstTmp).
-
-obtem_melhorMoto([Caminho],Caminho) :- !.
-obtem_melhorMoto([Caminho1/EstKm1/EstTmp1,_/EstKm2/EstTmp2|Caminhos],MelhorCaminho) :-
-    Produtividade1 is 0.3 * EstKm1 + 0.7 * EstTmp1 ,Produtividade2 is 0.3 * EstKm2 + 0.7 * EstTmp2 ,
-    Produtividade1 =< Produtividade2 , !,
-    obtem_melhorMoto([Caminho1/EstKm1/EstTmp1|Caminhos],MelhorCaminho).
-obtem_melhorMoto([_|Caminhos],MelhorCaminho) :-
-    obtem_melhorMoto(Caminhos,MelhorCaminho).
+    exactHeuristicMoto(NodoI,ProxNodo,Peso,EstKm,EstTmp).
 
 %------------------------------------------------------------------------------------------------
 %---------------------------------------------BICICLETA------------------------------------------
 
-agulosaBic(Caminhos,Caminho) :-
+agulosaBic2(Caminhos,Caminho,NodoI,_) :-
     obtem_melhorBic(Caminhos,Caminho),
-    Caminho = [Nodo|_]/_/_,
-    final(Nodo).
-agulosaBic(Caminhos,SolucaoCaminho) :-
+    Caminho = [NodoI|_]/_/_.
+agulosaBic2(Caminhos,SolucaoCaminho,NodoI,Peso) :-
     obtem_melhorBic(Caminhos,MelhorCaminho),
     seleciona(MelhorCaminho,Caminhos,OutrosCaminhos),
-    expande_gulosaBic(MelhorCaminho,ExpCaminhos),
+    expande_gulosaBic2(MelhorCaminho,ExpCaminhos,NodoI,Peso),
     append(OutrosCaminhos,ExpCaminhos,NovoCaminhos),
-    agulosaBic(NovoCaminhos,SolucaoCaminho).
+    agulosaBic2(NovoCaminhos,SolucaoCaminho,NodoI,Peso).
 
-expande_gulosaBic(Caminho,ExpCaminhos) :-
-    findall(NovoCaminho, (adjacenteBic(Caminho,NovoCaminho)), ExpCaminhos).
+expande_gulosaBic2(Caminho,ExpCaminhos,NodoI,Peso) :-
+    findall(NovoCaminho, (adjacenteBic2(Caminho,NovoCaminho,NodoI,Peso)), ExpCaminhos).
 
-adjacenteBic([Nodo|Caminho]/_/_,[ProxNodo,Nodo|Caminho]/EstKm/EstTmp) :-
+adjacenteBic2([Nodo|Caminho]/_/_, [ProxNodo,Nodo|Caminho]/EstKm/EstTmp, NodoI, Peso) :-
     move(Nodo,ProxNodo,_),
     \+member(ProxNodo,Caminho),
-    estimaB(ProxNodo,EstKm,EstTmp).
-
-obtem_melhorBic([Caminho],Caminho) :- !.
-obtem_melhorBic([Caminho1/EstKm1/EstTmp1,_/EstKm2/EstTmp2|Caminhos],MelhorCaminho) :-
-    Produtividade1 is 0.8 * EstKm1 + 0.2 * EstTmp1 ,Produtividade2 is 0.8 * EstKm2 + 0.2 * EstTmp2 ,
-    Produtividade1 =< Produtividade2 , !,
-    obtem_melhorBic([Caminho1/EstKm1/EstTmp1|Caminhos],MelhorCaminho).
-obtem_melhorBic([_|Caminhos],MelhorCaminho) :-
-    obtem_melhorBic(Caminhos,MelhorCaminho).
-
-*/
+    exactHeuristicBic(NodoI,ProxNodo,Peso,EstKm,EstTmp).
